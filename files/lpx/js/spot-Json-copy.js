bbcdbb87f61f4fe0ab2a25d291b30e5f9b602b27
Amlabort <34742444+Amlabort@users.noreply.github.com>
2026-03-01 17:25:27 +0800

    Update spot-Json-copy.js

--- files/lpx/js/spot-Json-copy.js
+++ files/lpx/js/spot-Json-copy.js
@@ -7,11 +7,13 @@ let headers = $request.headers;
 // 1. 处理端口号
 if (url.includes('com:443')) {
     url = url.replace(/com:443/, 'com');
+    console.log(`处理端口号`);
 }
 
 // 2. 伪装平台参数
 if (url.includes('platform=iphone')) {
     url = url.replace(/platform=iphone/, 'platform=ipad');
+    console.log(`伪装平台`);
     
     // 3. 同步修改 User-Agent，防止身份分裂导致掉号
     for (let key in headers) {
@@ -21,10 +23,11 @@ if (url.includes('platform=iphone')) {
             break;
         }
     }
-    console.log('已将请求伪装为 iPad 平台');
+    console.log('伪装User-Agent');
 }
 
 $done({
     url,
     headers
 });
+
