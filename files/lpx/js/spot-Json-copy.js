4feb9092505292c1f6daddea4410a1a0caca9686
Amlabort <34742444+Amlabort@users.noreply.github.com>
2026-03-01 20:57:44 +0800

    Update spot-Json-copy.js

--- files/lpx/js/spot-Json-copy.js
+++ files/lpx/js/spot-Json-copy.js
@@ -1,33 +1,26 @@
-// spotify-json-refined.js
-console.log(`spotify-json-2026.03.01`);
+ console.log(`spotify-json-ipad`);
 
 let url = $request.url;
-let headers = $request.headers;
 
-// 1. 处理端口号
-if (url.includes('com:443')) {
-    url = url.replace(/com:443/, 'com');
-    console.log(`处理端口号`);
+// console.log(`原始url:${url}`);
+
+if (url.includes('com: 443')) {
+
+url = url.replace(/com: 443/, 'com');
 }
 
-// 2. 伪装平台参数
 if (url.includes('platform=iphone')) {
-    url = url.replace(/platform=iphone/, 'platform=ipad');
-    console.log(`伪装平台`);
-    
-    // 3. 同步修改 User-Agent，防止身份分裂导致掉号
-    for (let key in headers) {
-        if (key.toLowerCase() === 'user-agent') {
-            // 将 iPhone 关键字替换为 iPad
-            headers[key] = headers[key].replace(/iPhone/g, 'iPad');
-            break;
-        }
-    }
-    console.log('伪装User-Agent');
+
+url = url.replace(/platform=iphone/, 'platform=ipad');
+
+// console.log(`替换platform:${url}`);
+} else {
+
+console.log('无需处理');
 }
 
 $done({
-    url,
-    headers
-});
+url
+}); 
+
 
