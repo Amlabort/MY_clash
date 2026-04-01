7d5626637f54370d80b2a62ba2c04c54d999f9d3
Amlabort <34742444+Amlabort@users.noreply.github.com>
2026-04-01 10:49:27 +0800

    Update spot-OldVer.js

--- files/lpx/js/spot-OldVer.js
+++ files/lpx/js/spot-OldVer.js
@@ -1,6 +1,6 @@
 let headers = $request.headers;
-if(headers["spotify-app-version"]) { headers["spotify-app-version"] = "9.1.14.864"; }
-if(headers["user-agent"]) { headers["user-agent"] = "Spotify/9.1.14.864 iOS/Version 26.4 (Build 23E246)"; } 
+if(headers["spotify-app-version"]) { headers["spotify-app-version"] = "9.1.12.224"; }
+if(headers["user-agent"]) { headers["user-agent"] = "Spotify/9.1.12.224 iOS/Version 26.4 (Build 23E246)"; } 
 [
     "spotify-dsa-mode-enabled",
     "spotify-apply-lenses",
