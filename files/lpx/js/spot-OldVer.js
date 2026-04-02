f0bb7e575a9fb2c27ecc5d6c21f581619b59207c
Amlabort <34742444+Amlabort@users.noreply.github.com>
2026-04-02 15:17:25 +0800

    Update spot-OldVer.js

--- files/lpx/js/spot-OldVer.js
+++ files/lpx/js/spot-OldVer.js
@@ -1,13 +1,7 @@
+const resStatus = $response.status ? $response.status : $response.statusCode;
+if(resStatus !== 200) {
+    esponse.status = 200;
+    response.body = JSON.stringify({});
+}
+$done(response);
 
-$done((response) => {
-    let code = Number(response.statusCode);
-    // 判断状态码是否是 404 或 403
-    if (code === 404 || code === 403) {
-        // 改为 200
-        response.statusCode = 200;
-        response.body = JSON.stringify({});
-    }
-
-    // 其他状态码保持原样
-    $done(response);
-});
