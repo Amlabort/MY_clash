3f920d1408de7ee70565b1b0f38275d23b7ff1a2
Amlabort <34742444+Amlabort@users.noreply.github.com>
2026-04-02 15:25:33 +0800

    Update spot-OldVer.js

--- files/lpx/js/spot-OldVer.js
+++ files/lpx/js/spot-OldVer.js
@@ -1,7 +1,8 @@
 const resStatus = $response.status ? $response.status : $response.statusCode;
 if(resStatus !== 200) {
-    esponse.status = 200;
-    response.body = JSON.stringify({});
+    console.log(`$response.status不为200:${resStatus}`);
+    $response.status = 200;
+    $response.body = JSON.stringify({});
 }
-$done(response);
+$done($response);
 
