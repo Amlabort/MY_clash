4a01344581e3d951abf157d600d2ab883ac2a2f0
Amlabort <34742444+Amlabort@users.noreply.github.com>
2026-04-02 15:09:35 +0800

    Update spot-OldVer.js

--- files/lpx/js/spot-OldVer.js
+++ files/lpx/js/spot-OldVer.js
@@ -1,7 +1,8 @@
 
 $done((response) => {
+    let code = Number(response.statusCode);
     // 判断状态码是否是 404 或 403
-    if (response.statusCode === 404 || response.statusCode === 403) {
+    if (code === 404 || code === 403) {
         // 改为 200
         response.statusCode = 200;
         response.body = JSON.stringify({});
