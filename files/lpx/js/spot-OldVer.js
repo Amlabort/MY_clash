ccf5de1c2bccbe63412d0a30773a7eeff86e833c
Amlabort <34742444+Amlabort@users.noreply.github.com>
2026-04-14 16:12:26 +0800

    Update and rename spot-OldVer.js to spot-proto-eevee.js

--- files/lpx/js/spot-OldVer.js
+++
@@ -1,8 +0,0 @@
-const resStatus = $response.status ? $response.status : $response.statusCode;
-if(resStatus !== 200) {
-    console.log(`$response.status不为200:${resStatus}`);
-    $response.status = 200;
-    //$response.body = JSON.stringify({});
-}
-$done($response);
-
