c793110a44ac384014d1aa463087a1dded8229bd
Amlabort <34742444+Amlabort@users.noreply.github.com>
2026-04-20 13:59:34 +0800

    Update spot-NoScrollAd.js

--- files/lpx/js/spot-NoScrollAd.js
+++ files/lpx/js/spot-NoScrollAd.js
@@ -1,22 +1,24 @@
 // 获取二进制响应体
-let body = $response.body;
+let body = new Uint8Array($response.body);
 
-// 定义我们计算出的固定偏移量
-const targetOffset = 162; 
+let MAX=300;
+let flag=0;
 
-// 安全检查：验证该位置是否确实是 F2 01
-if (body && body.length > targetOffset + 1) {
-    if (body[targetOffset] === 0xF2 && body[targetOffset + 1] === 0x01) {
-        
-        // 执行替换：将 Tag 30 (F2 01) 修改为 Tag 1015 (F7 07)
+for (let i = 0; i < MAX; i++) {
+    if (body[i] === 0xF2 && body[i + 1] === 0x01) {
 
-        body[targetOffset] = 0xF7;
-        body[targetOffset + 1] = 0x07;
-        
-        console.log("成功在偏移量 162 处更改tag值");
-    } else {
-        console.log("未检测到广告" );
+        body[i] = 0xF7;
+        body[i + 1] = 0x07;
+        console.log(`成功在偏移量 ${i} 处更改tag值`);
+        flag=1;
+        break;
     }
 }
 
-$done({ body });
+if(!flag) console.log(`未检测到广告`);
+
+
+//$done({ body });
+$done({ body: body.buffer });
+
+
