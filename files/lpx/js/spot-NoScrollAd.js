5dc0a517e59557650f6c51b12240fc25379a4ac4
Amlabort <34742444+Amlabort@users.noreply.github.com>
2026-04-20 14:09:20 +0800

    Update spot-NoScrollAd.js

--- files/lpx/js/spot-NoScrollAd.js
+++ files/lpx/js/spot-NoScrollAd.js
@@ -1,5 +1,5 @@
 // 获取二进制响应体
-let body = new Uint8Array($response.body);
+let body = $response.body;
 
 let MAX=300;
 let flag=0;
@@ -11,14 +11,15 @@ for (let i = 0; i < MAX; i++) {
         body[i + 1] = 0x07;
         console.log(`成功在偏移量 ${i} 处更改tag值`);
         flag=1;
-        break;
+        $done({ body });
+        return;
     }
 }
 
 if(!flag) console.log(`未检测到广告`);
 
 
-//$done({ body });
-$done({ body: body.buffer });
+
+//$done({ body: body.buffer });
 
 
