4d66220297fdd5e899b3c2b2641e72bd7c8ec3db
Amlabort <34742444+Amlabort@users.noreply.github.com>
2026-08-10 22:42:21 +0800

    Update zhihu-NoAd.js

--- files/lpx/js/zhihu-NoAd.js
+++ files/lpx/js/zhihu-NoAd.js
@@ -1,52 +1,60 @@
+// let body = $response.body;
+
+// let flag = 0;
+
+// console.log(`HomeNoAd`);
+
+// try {
+
+//     let obj = JSON.parse(body);
+
+
+//     if (obj.data && Array.isArray(obj.data)) {
+
+//         // 删除 data 前5项中的广告字段
+//         for (let i = 0; i < Math.min(5, obj.data.length); i++) {
+
+//             if (obj.data[i].ad_info) {
+//                 delete obj.data[i].ad_info;
+//                 flag = 1;
+//                 console.log(`删除 data[${i}].ad_info`);
+//             }
+
+
+//             if (obj.data[i].adjson) {
+//                 delete obj.data[i].adjson;
+//                 flag = 1;
+//                 console.log(`删除 data[${i}].adjson`);
+//             }
+
+//         }
+
+//     }
+
+
+//     if (flag) {
+//         console.log(`去广告成功`);
+//         $done({
+//             body: JSON.stringify(obj)
+//         });
+//     }
+//     else {
+//         console.log(`未检测到广告`);
+//         $done({});
+//     }
+
+
+// }
+// catch(e) {
+
+//     console.log(`JSON解析失败 ${e}`);
+//     $done({});
+// }
+
+
 let body = $response.body;
 
-let flag = 0;
+console.log("长度：" + body.length);
+console.log("前200字符：" + body.substring(0, 200));
 
-console.log(`HomeNoAd`);
-
-try {
-
-    let obj = JSON.parse(body);
-
-
-    if (obj.data && Array.isArray(obj.data)) {
-
-        // 删除 data 前5项中的广告字段
-        for (let i = 0; i < Math.min(5, obj.data.length); i++) {
-
-            if (obj.data[i].ad_info) {
-                delete obj.data[i].ad_info;
-                flag = 1;
-                console.log(`删除 data[${i}].ad_info`);
-            }
-
-
-            if (obj.data[i].adjson) {
-                delete obj.data[i].adjson;
-                flag = 1;
-                console.log(`删除 data[${i}].adjson`);
-            }
-
-        }
-
-    }
-
-
-    if (flag) {
-        console.log(`去广告成功`);
-        $done({
-            body: JSON.stringify(obj)
-        });
-    }
-    else {
-        console.log(`未检测到广告`);
-        $done({});
-    }
-
-
-}
-catch(e) {
-
-    console.log(`JSON解析失败 ${e}`);
-    $done({});
-}
+$done({});
